0edff2a12dd05daad87f868a7b43350ce0bde15e
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2026-05-18 19:23:07 +0000

    MAJ

--- src/Controller/Admin/FooterAproposCrudController.php
+++ src/Controller/Admin/FooterAproposCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\FooterApropos;
 use App\Repository\FooterAproposRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -17,12 +18,15 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class FooterAproposCrudController extends AbstractCrudController
 {
     public function __construct(
         private AdminUrlGenerator $adminUrlGenerator,
-        private readonly FooterAproposRepository $aproposRepository
+        private readonly FooterAproposRepository $aproposRepository,
+        private CacheInterface $cache,           // ← Injection
     )
     {
     }
@@ -77,5 +81,23 @@ class FooterAproposCrudController extends AbstractCrudController
             TextField::new('tags', )
         ];
     }
+
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        parent::updateEntity($entityManager, $entityInstance);
+        $this->invalidateFooterCache();
+    }
+
+    private function invalidateFooterCache(): void
+    {
+        if ($this->cache instanceof TagAwareCacheInterface) {
+            $this->cache->invalidateTags(['footer', 'apropos']);
+        } else {
+            // Fallback si pas de tags
+            $this->cache->delete('footer_presentation');
+        }
+    }
+
 
 }
